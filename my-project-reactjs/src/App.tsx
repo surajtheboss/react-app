8efd1b2632b95048d4782850dfd459112bed55fd
sshahu8055 <sshahu@valethi.in>
2026-06-20 12:42:08 +0530

    intial comment

---
+++ my-project-reactjs/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="mb-4">
+        Hello World
+      </h1>
+      <p>
+        suraj shahu
+      </p>
+    </>
+  )
+}
+
+export default App
